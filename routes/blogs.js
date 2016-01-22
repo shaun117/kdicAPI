0e2d40e3f88e422867cacb32161dcb69bf003cb8
shaun117 <shaun.mataire@live.com>
2016-01-22 14:08:49 -0600

    got html object

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,9 +1,45 @@
+//
+//  blogs.js
+//  kdicAPI
+//
+//  Created by Shaun Mataire on 1/14/16.
+//  Copyright © 2016 AppDev. All rights reserved.
+//
+
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
 
-/* GET users listing. */
+/* GET blogs home. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  var blogsUrl = 'http://kdic.grinnell.edu'
+
+  request(blogsUrl, function(error, response, html){
+    //check whether there was an error making the request
+    if(!error){
+      //get the cheerio html object
+      var $ = cheerio.load(html);
+
+      //json result = articles object that is an array of json objects
+      var blogsJson = {
+        "articles": []
+      }
+
+      //we'' use the unique header as a starting point
+      $('div .posts-layout').filter(function(){
+
+        //get the html object data
+        var data = $(this)
+
+        //debug
+        console.log(data.html());
+      });
+
+    }
+  });
+  res.send('200');
 });
 
 module.exports = router;
